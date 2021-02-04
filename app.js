7a988a1213adf04dda313c18545494eafe71164f
zsoltelekes09 <elekeszss@stud.uni-obuda.hu>
2021-02-05 00:48:15 +0100

    changed port to be compatible with heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var path = require("path");
 const bodyParser= require('body-parser')
-const port = 8080
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
